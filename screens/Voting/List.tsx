467743eca115d96a95eb20a5ebb786fd8efa96d4
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-12 00:32:41 -0600

    refactor(Root): flatlist
    solve perf issues

--- screens/Voting/List.tsx
+++ screens/Voting/List.tsx
@@ -2,42 +2,108 @@ import React, { useState } from "react";
 import {
   FlatList,
   GestureResponderEvent,
+  Image,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableNativeFeedback,
   TouchableOpacity,
+  View,
 } from "react-native";
 import movies from "../../data/30-movies.json";
 
 type ItemProps = {
-  item: any;
+  itemId: string;
+  itemUrl: string;
+  itemTitle: string;
   onPress: (event: GestureResponderEvent) => void;
-  textColor: string;
-  backgroundColor: string;
+  isSelected: boolean;
 };
 
-const Item = ({ item, onPress, backgroundColor, textColor }: ItemProps) => (
-  <TouchableOpacity
-    className={`${backgroundColor} p-8 my-4 mx-4`}
-    onPress={onPress}
-  >
-    <Text className={`${textColor} text-2xl`}>{item.title}</Text>
-  </TouchableOpacity>
-);
+const Item = (
+  { itemId, itemUrl, itemTitle, onPress, isSelected }: ItemProps,
+) => {
+  // if ios
+  // return (
+  //     <TouchableOpacity
+  //       key={item.title}
+  //       className={`${backgroundColor} p-8 my-4 mx-4`}
+  //       onPress={onPress}
+  //     >
+  //       {/**
+  //        * for local images <Image> reads the metadata
+  //        * hence you dont need to pass the styles
+  //        * fadeDuration - android only!
+  //        * @see https://reactnative.dev/docs/image
+  //        **/}
+  //       <Image className="w-24 h-24 bg-black rounded-md"  resizeMode="contain" fadeDuration={1000} blurRadius={6} source={{ uri: item.photoUrL }} />
+  //       <Text className={`${textColor} text-2xl pt-4`}>{item.title}</Text>
+  //     </TouchableOpacity>
+  //   )
+  // else android
+  console.log("<item>", itemId);
+
+  return (
+    <TouchableNativeFeedback key={itemTitle} onPress={onPress}>
+      <View
+        className={`${
+          isSelected ? "bg-purple-800" : "bg-blue-200"
+        } p-8 my-4 mx-4`}
+      >
+        {
+          /**
+           * for local images <Image> reads the metadata
+           * hence you dont need to pass the styles
+           * fadeDuration - android only!
+           * @see https://reactnative.dev/docs/image
+           */
+        }
+        <Image
+          className="w-24 h-24 bg-black rounded-md"
+          resizeMode="contain"
+          fadeDuration={1000}
+          blurRadius={6}
+          source={{ uri: itemUrl }}
+        />
+        <Text
+          className={`${
+            isSelected ? "text-white" : "text-black"
+          } text-2xl pt-4`}
+        >
+          {itemTitle}
+        </Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+};
+
+const areEqual = (prevProps: any, nextProps: any) => {
+//   console.log("==?");
+  const { isSelected } = nextProps;
+  const { isSelected: prevIsSelected } = prevProps;
+  /*if the props are equal, it won't update*/
+  const isSelectedEqual = isSelected == prevIsSelected;
+  return isSelectedEqual;
+};
+
+const MemoItem = React.memo(Item, areEqual);
+const keyExtractor = (item: any) => item.id;
 
 export function List() {
+  console.log("<List>");
   const [selectedId, setSelectedId] = useState(null);
 
   const renderItem = ({ item }: { item: any }) => {
     return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={item.id == selectedId
-          ? "bg-purple-800"
-          : "bg-pink-200"}
-        textColor={item.id == selectedId ? "text-white" : "text-black"}
+      <MemoItem
+        itemId={item.id}
+        itemTitle={item.title}
+        itemUrl={item.photoUrL}
+        onPress={() => {
+          setSelectedId(item.id);
+        }}
+        isSelected={item.id == selectedId}
       />
     );
   };
@@ -47,7 +113,7 @@ export function List() {
       <FlatList
         data={movies.movies}
         renderItem={renderItem}
-        keyExtractor={(item) => item.title}
+        keyExtractor={keyExtractor}
         extraData={selectedId}
       />
     </SafeAreaView>
--- screens/Voting/Main.tsx
+++ screens/Voting/Main.tsx
@@ -5,7 +5,7 @@ import { List } from "./List";
 export function Voting() {
   return (
     <SafeAreaView className="items-center justify-center flex-1 bg-blue-400">
-      <Text className="text-slate-200">Voting ssyetms</Text>
+      <Text className="pt-4 text-xl text-slate-200">Voting syetms</Text>
       <Text className="text-slate-200" numberOfLines={1}>
         A very long and text let see what happens over here!, let's add a bit
         more text
