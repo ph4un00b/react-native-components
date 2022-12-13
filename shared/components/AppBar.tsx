1d0e05ef7b4d492e39bbe516d363000b2d0f0542
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-12 21:19:47 -0600

    fix(Root): status bar

--- shared/components/AppBar.tsx
+++ shared/components/AppBar.tsx
@@ -1,6 +1,7 @@
 import { ScrollView, Text, View } from "react-native";
 import { Link, useLocation } from "react-router-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 type TabProps = { children: React.ReactNode; to: string };
 
@@ -20,6 +21,7 @@ function AppBarTab({ children, to }: TabProps) {
 export function AppBar() {
   return (
     <>
+      <StatusBar style="light" />
       <View className="flex flex-row w-full pt-6 bg-slate-600">
         <ScrollView horizontal className="pb-8">
           <AppBarTab to="/">Voting system</AppBarTab>
