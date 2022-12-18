2f97c3faaeb2247bf985386beacfd793de901948
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-17 20:15:07 -0600

    chore(Root): import packages

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ import { NativeRouter } from "react-router-native";
 /** ensure that appropriate event handlers are registered with React Native */
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { probando } from "@phau/probando/src";
 
+console.log({ probando });
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
