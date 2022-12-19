56b751d6bf70b58ead14858608dbf4aaacaf999f
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-19 13:28:33 -0600

    refactor(Root): <routes>

--- App.tsx
+++ App.tsx
@@ -1,17 +1,54 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { Text, View, TouchableNativeFeedback, Alert } from 'react-native';
 import "./generated/nativewind-output";
-import { AppRoutes } from "./screens/Voting/AppRoutes";
-import { NativeRouter } from "react-router-native";
+import { NativeRouter, Route, Routes } from "react-router-native";
 /** ensure that appropriate event handlers are registered with React Native */
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native";
+import { AppBar } from "./shared/components/AppBar";
+import { MenuPage } from "./screens/Indexes/Menu";
+import { DrawerPage } from "./screens/UX/Drawer";
+import { NativeImagePage } from "./screens/Native/Image";
+import { ListingPage } from "./screens/Indexes/Listing";
+import { ListPage } from "./screens/Voting/List";
+import { SimpleSwipeListPage } from "./screens/Voting/SimpleSwipableList";
+import { SwipeListInsidePage } from "./screens/Voting/SwipableListInside";
+import { ClassSwipableListPage } from "./screens/Voting/ClassSwipableList";
+import { SwipeListPage } from "./screens/Voting/SwipeList";
+import { LoginPage } from "./screens/Login/Login";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NativeRouter>
-        <AppRoutes />
+        <SafeAreaView className="flex items-center justify-center flex-1 bg-slate-800">
+          <AppBar />
+          {/**
+           * All <Route>s and <Link>s inside a <Routes> are relative.
+           *  This leads to leaner and more predictable code in <Route path>
+           *  and <Link to>
+           * Routes are chosen based on the best match instead of being traversed
+           * in order. This avoids bugs due to unreachable routes because they
+           * were defined later in your <Switch>
+           * Routes may be nested in one place instead of being spread out
+           * in different components. In small to medium-sized apps, this
+           *  lets you easily see all your routes at once. In large apps,
+           * you can still nest routes in bundles that you load dynamically
+           * via React.lazy
+           * @see https://reactrouter.com/en/6.4.5/upgrading/v5#upgrade-all-switch-elements-to-routes
+           */}
+          <Routes>
+            <Route path="/" element={<MenuPage />} />
+            <Route path="/drawer" element={<DrawerPage />} />
+            <Route path="/native-image" element={<NativeImagePage />} />
+            <Route path="/listing" element={<ListingPage />} />
+            <Route path="/list" element={<ListPage />} />
+            <Route path="/swipe-simple" element={<SimpleSwipeListPage />} />
+            <Route path="/swipe-inside" element={<SwipeListInsidePage />} />
+            <Route path="/swipe-class" element={<ClassSwipableListPage />} />
+            <Route path="/swipe-outside" element={<SwipeListPage />} />
+            <Route path="/signin" element={<LoginPage />} />
+          </Routes>
+        </SafeAreaView>
       </NativeRouter>
     </GestureHandlerRootView>
   );
