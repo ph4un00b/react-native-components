2cdaec8faa59a071227e47b17292688c54074247
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-23 14:46:07 -0600

    fix(Root): linking URI issues

--- screens/linking.tsx
+++ screens/linking.tsx
@@ -4,33 +4,54 @@ import * as Linking from "expo-linking";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-native";
 
-const redirectUrl = Linking.createURL("home", {
+const redirectUrl = Linking.createURL("/home", {
   queryParams: { hello: "world" },
 });
 
 // Linking.addEventListener("url", (e) => console.log(e));
 export function LinkingScreen() {
   const navigate = useNavigate();
-  /** this hook uses
+  /**
+   * this hook 'Linking#useURL' uses
    * Linking.getInitialURL and
    * Linking.addEventListener('url', callback)
    *
-   * under the hook
-   * terminal cmd:
-   * $ pnpx uri-scheme open exp://192.168.1.110:19000/--/URI?QUERY=PARAM --android
+   * under the hood
+   *
+   * =========
+   * terminal expo-go test cmd:
+   * $ p|npx uri-scheme open exp://(YOUR_IP)192.168.1.110:19000/--/URI?QUERY=PARAM --android
    */
   const url = Linking.useURL();
 
   useEffect(() => {
     if (!url) return;
-    console.log({url})
+    console.log({ url });
+    /**
+     * So, there are three approaches you can take:
+     * If you have control over the URI,
+     *  add a third slash after the scheme (myapp:///hello/world)
+     *  so that the host portion of the URI is empty.
+     * If you have control over the URI,
+     *  remove the two slashes after the scheme
+     *  and use a URN instead of URL (myapp:hello/world).
+     * If you don't have control over the URI,
+     *  change your application code to join
+     *  together the parsed URL's host and path
+     *  with a slash ('hello' + '/' + 'world').
+     * @see https://github.com/expo/expo/issues/6497#issuecomment-574882448
+     */
     const { hostname, path, queryParams } = Linking.parse(url);
-    console.log({hostname,path,queryParams})
+    console.log({ hostname, path, queryParams });
     /**
      * if you don't add "/" at the begining
      * the path will be a sub-directory of the curent path
+     *
+     * the parsing process (Linking.parse(url)
+     * will remove the slash.
+     * make sure to prepend a new slash "/"
      */
-    console.log("trying to go:/" + path);
+    console.log("goingto:/" + path);
     path && navigate("/" + path);
     return () => console.log("bye");
   }, [url]);
@@ -39,7 +60,9 @@ export function LinkingScreen() {
     const { hostname, path, queryParams } = Linking.parse(url);
 
     Alert.alert(
-      `hostname ${hostname}, \npath: ${path}, \ndata: ${JSON.stringify(queryParams)}`
+      `hostname ${hostname}, \npath: ${path}, \ndata: ${JSON.stringify(
+        queryParams
+      )}`
     );
   }
 
