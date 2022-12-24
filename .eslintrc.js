4118f436112f83c38d29da183355322d215444a8
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-24 13:47:37 -0600

    chore(Root): react-native tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,40 @@
-export default {
-    "env": {
-        "browser": true,
-        "es2021": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    /** if problems you might use @see https://www.npmjs.com/package/@react-native-community/eslint-plugin */
+    "universe/native",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.d.ts"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  ],
+  settings: {
+    // "import/ignore": ["node_modules/react-native/index\\.js$"],
+    "import/ignore": ["react-native"],
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  rules: {
+    eqeqeq: ["off", "smart"],
+    "react/no-unescaped-entities": ["warn", { forbid: ["'"] }],
+    "react-hooks/exhaustive-deps": "error",
+  },
+};
