314e15f183e47b9418acecf914a46e5dcebcc350
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-12-24 23:35:07 -0600

    chore(Root): todo for web

---
+++ utils/secureStorage.web.ts
@@ -0,0 +1,11 @@
+export async function store({ key, value }: { key: string; value: string }) {
+  throw new Error("implement me@!");
+}
+
+export async function request({ key }: { key: string }) {
+  throw new Error("implement me@!");
+}
+
+export async function remove({ key }: { key: string }) {
+  throw new Error("implement me@!");
+}
