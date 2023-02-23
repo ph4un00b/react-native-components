40a04dd0ad07c638c7d9f8baa4a5aca2c4a11e2f
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-23 16:20:44 -0600

    lint

--- screens/Animations/declarative.skia.tsx
+++ screens/Animations/declarative.skia.tsx
@@ -2,27 +2,27 @@ import { Canvas, Circle, Group, SkiaView } from "@shopify/react-native-skia";
 import { View } from "react-native";
 
 export function SkiaCanvas() {
-  const size = 256;
-  const r = size * 0.33;
-  return (
-    <View
-      style={{
-        // width: width - 24,
-        flexGrow: 1,
-        backgroundColor: "#ffffff",
-        borderRadius: 10,
-        overflow: "hidden",
-        elevation: 1,
-      }}
-    >
-      <SkiaView style={{ zIndex: 10 }} />
-      <Canvas style={{ flex: 1 }}>
-        <Group blendMode="multiply">
-          <Circle cx={r} cy={r} r={r} color="cyan" />
-          <Circle cx={size - r} cy={r} r={r} color="magenta" />
-          <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
-        </Group>
-      </Canvas>
-    </View>
-  );
+	const size = 256;
+	const r = size * 0.33;
+	return (
+		<View
+			style={{
+				// width: width - 24,
+				flexGrow: 1,
+				backgroundColor: "#ffffff",
+				borderRadius: 10,
+				overflow: "hidden",
+				elevation: 1,
+			}}
+		>
+			<SkiaView style={{ zIndex: 10 }} />
+			<Canvas style={{ flex: 1 }}>
+				<Group blendMode="multiply">
+					<Circle cx={r} cy={r} r={r} color="cyan" />
+					<Circle cx={size - r} cy={r} r={r} color="magenta" />
+					<Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
+				</Group>
+			</Canvas>
+		</View>
+	);
 }
--- utils/auth.tsx
+++ utils/auth.tsx
@@ -6,142 +6,142 @@ import { removeToken } from "./auth.store";
 type User = { token?: string; name?: string; image?: string };
 // type AuthContextType = [User, Dispatch<SetStateAction<User>>];
 type AuthContextType = {
-  user: User | null;
-  login: ({
-    token,
-    callback,
-  }: {
-    token: string;
-    callback?: VoidFunction;
-  }) => void;
-  saveUser: (user: User, callback?: VoidFunction) => void;
-  logout: (callback?: VoidFunction) => void;
+	user: User | null;
+	login: ({
+		token,
+		callback,
+	}: {
+		token: string;
+		callback?: VoidFunction;
+	}) => void;
+	saveUser: (user: User, callback?: VoidFunction) => void;
+	logout: (callback?: VoidFunction) => void;
 };
 
 const AuthContext = createContext<AuthContextType>(null!);
 
 export function useAuth() {
-  const value = useContext(AuthContext);
-  if (value == null) throw new Error("Provider missing!");
+	const value = useContext(AuthContext);
+	if (value == null) throw new Error("Provider missing!");
 
-  return value;
+	return value;
 }
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
+	const [user, setUser] = useState<User | null>(null);
 
-  type LoginProps = {
-    token: string;
-    callback?: VoidFunction;
-  };
+	type LoginProps = {
+		token: string;
+		callback?: VoidFunction;
+	};
 
-  function login({ token, callback }: LoginProps) {
-    console.log("💪");
-    const opts = { method: "GET", headers: githubHeaders(token) };
-    fetch("https://api.github.com/user", opts)
-      .then((res) => jsonOrError(res, "@github/user"))
-      .then((data: GithubProfile) => {
-        setUser({
-          name: data.login ?? data.name,
-          token,
-          image: data.avatar_url,
-        });
-      })
-      .then(() => {
-        callback?.();
-        console.log("login-ready!! 💃");
-      })
-      .catch(console.warn);
-  }
+	function login({ token, callback }: LoginProps) {
+		console.log("💪");
+		const opts = { method: "GET", headers: githubHeaders(token) };
+		fetch("https://api.github.com/user", opts)
+			.then((res) => jsonOrError(res, "@github/user"))
+			.then((data: GithubProfile) => {
+				setUser({
+					name: data.login ?? data.name,
+					token,
+					image: data.avatar_url,
+				});
+			})
+			.then(() => {
+				callback?.();
+				console.log("login-ready!! 💃");
+			})
+			.catch(console.warn);
+	}
 
-  function logout(callback?: VoidFunction) {
-    setUser({});
-    removeToken();
-    callback?.();
-  }
+	function logout(callback?: VoidFunction) {
+		setUser({});
+		removeToken();
+		callback?.();
+	}
 
-  function saveUser(user: User) {
-    setUser(user);
-  }
+	function saveUser(user: User) {
+		setUser(user);
+	}
 
-  const value = { user, login, logout, saveUser };
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	const value = { user, login, logout, saveUser };
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export function RequireAuth({ children }: { children: JSX.Element }) {
-  const auth = useAuth();
-  const location = useLocation();
+	const auth = useAuth();
+	const location = useLocation();
 
-  if (!auth.user?.token) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
-    console.log("=== saving ===", location);
-    return <Navigate to="/github" state={{ from: location }} replace />;
-  }
-  return children;
+	if (!auth.user?.token) {
+		// Redirect them to the /login page, but save the current location they were
+		// trying to go to when they were redirected. This allows us to send them
+		// along to that page after they login, which is a nicer user experience
+		// than dropping them off on the home page.
+		console.log("=== saving ===", location);
+		return <Navigate to="/github" state={{ from: location }} replace />;
+	}
+	return children;
 }
 
 function githubHeaders(aToken: string): HeadersInit {
-  return {
-    Accept: "application/vnd.github+json",
-    Authorization: `Bearer ${aToken}`,
-    "X-GitHub-Api-Version": "2022-11-28",
-  };
+	return {
+		Accept: "application/vnd.github+json",
+		Authorization: `Bearer ${aToken}`,
+		"X-GitHub-Api-Version": "2022-11-28",
+	};
 }
 
 function jsonOrError(res: Response, err: string) {
-  if (!res.ok) throw new Error("something went wrong " + err);
-  return res.json();
+	if (!res.ok) throw new Error("something went wrong " + err);
+	return res.json();
 }
 
 /** copied from @see https://github.com/nextauthjs/next-auth/blob/dcb601987bb050f540c137440514ad54728ed801/packages/core/src/providers/github.ts */
 /** @see https://docs.github.com/en/rest/users/users#get-the-authenticated-user */
 export interface GithubProfile extends Record<string, unknown> {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string | null;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
-  name: string | null;
-  company: string | null;
-  blog: string | null;
-  location: string | null;
-  email: string | null;
-  hireable: boolean | null;
-  bio: string | null;
-  twitter_username?: string | null;
-  public_repos: number;
-  public_gists: number;
-  followers: number;
-  following: number;
-  created_at: string;
-  updated_at: string;
-  private_gists?: number;
-  total_private_repos?: number;
-  owned_private_repos?: number;
-  disk_usage?: number;
-  suspended_at?: string | null;
-  collaborators?: number;
-  two_factor_authentication: boolean;
-  plan?: {
-    collaborators: number;
-    name: string;
-    space: number;
-    private_repos: number;
-  };
+	login: string;
+	id: number;
+	node_id: string;
+	avatar_url: string;
+	gravatar_id: string | null;
+	url: string;
+	html_url: string;
+	followers_url: string;
+	following_url: string;
+	gists_url: string;
+	starred_url: string;
+	subscriptions_url: string;
+	organizations_url: string;
+	repos_url: string;
+	events_url: string;
+	received_events_url: string;
+	type: string;
+	site_admin: boolean;
+	name: string | null;
+	company: string | null;
+	blog: string | null;
+	location: string | null;
+	email: string | null;
+	hireable: boolean | null;
+	bio: string | null;
+	twitter_username?: string | null;
+	public_repos: number;
+	public_gists: number;
+	followers: number;
+	following: number;
+	created_at: string;
+	updated_at: string;
+	private_gists?: number;
+	total_private_repos?: number;
+	owned_private_repos?: number;
+	disk_usage?: number;
+	suspended_at?: string | null;
+	collaborators?: number;
+	two_factor_authentication: boolean;
+	plan?: {
+		collaborators: number;
+		name: string;
+		space: number;
+		private_repos: number;
+	};
 }
